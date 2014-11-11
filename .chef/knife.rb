a1a071a3dae8a6b1c78aa1feb202e4ca61b1f4ac
aniruddhaGQ <aniruddha.w@gquotient.com>
2014-11-11 19:41:04 +0530

    initial commit from chef-repo
    initial commit from chef-repo

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'aniruddha'
+client_key               'C:/Users/aniruddha wani.GQUOTIENT/chef-repo/.chef/aniruddha.pem'
+validation_client_name   'chef-validator'
+validation_key           'C:/Users/aniruddha wani.GQUOTIENT/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://54.169.137.216'
+syntax_check_cache_path  'C:/Users/aniruddha wani.GQUOTIENT/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ 'C:/Users/aniruddha wani.GQUOTIENT/chef-repo/cookbooks' ]
